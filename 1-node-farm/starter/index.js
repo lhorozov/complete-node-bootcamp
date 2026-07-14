1c95d5a0eff21c09c8fa2f0b18fbac972f5ce61b
Lyuben Horozov <lhorozov@yahoo.com>
2026-07-14 23:05:35 +0300

    parsing variables from urls

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 // ------------FILES------------
 // // Blocking synchronous way
@@ -57,23 +58,30 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObject = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const path = req.url;
+  const { pathname, query } = url.parse(req.url, true);
 
   // Overview page
-  if (path === "/" || path === "/overview") {
+  if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, {
       "Content-type": "text/html",
     });
-    const cardsHtml = dataObject.map((el) => replaceTemplate(templateCard, el)).join('');
-    const output = templateOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    const cardsHtml = dataObject
+      .map((el) => replaceTemplate(templateCard, el))
+      .join("");
+    const output = templateOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
     res.end(output);
 
     // Product page
-  } else if (path === "/product") {
-    res.end("This is the product");
+  } else if (pathname === "/product") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    const product = dataObject[query.id];
+    const output = replaceTemplate(templateProduct, product);
+    res.end(output);
 
     // API
-  } else if (path === "/api") {
+  } else if (pathname === "/api") {
     res.writeHead(200, {
       "Content-type": "application/json",
     });

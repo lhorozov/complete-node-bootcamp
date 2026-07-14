681e44fa137c2c6b425cc685ec7a6d50597efd54
Lyuben Horozov <lhorozov@yahoo.com>
2026-07-14 21:51:35 +0300

    add api endpoint

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -25,7 +25,30 @@ const http = require("http");
 // ------------FILES------------
 
 // ------------SERVER------------
-http.createServer((req, res) => {
-  res.end("Hello from the server!");
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObject = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const path = req.url;
+  if (path === "/" || path === "/overview") {
+    res.end("This is the overview");
+  } else if (path === "/product") {
+    res.end("This is the product");
+  } else if (path === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found<h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
 });
 // ------------SERVER------------

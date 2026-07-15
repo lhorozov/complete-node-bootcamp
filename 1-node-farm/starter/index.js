4bb93c17be140f6a060ffde7354df8741233f170
Lyuben Horozov <lhorozov@yahoo.com>
2026-07-15 13:47:54 +0300

    add modules folder

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const replaceTemplate = require('./modules/replaceTemplate');
 
 // ------------FILES------------
 // // Blocking synchronous way
@@ -26,21 +27,7 @@ const url = require("url");
 // ------------FILES------------
 
 // ------------SERVER------------
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
 
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/, "not-organic");
-
-  return output;
-};
 const templateCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
   "utf-8",

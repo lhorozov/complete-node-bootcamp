6b08cd2709d2a2e5639d2e472ffb5ed649664bed
Lyuben Horozov <lhorozov@yahoo.com>
2026-07-15 13:54:17 +0300

    add package.json

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -3,31 +3,6 @@ const http = require("http");
 const url = require("url");
 const replaceTemplate = require('./modules/replaceTemplate');
 
-// ------------FILES------------
-// // Blocking synchronous way
-// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-
-// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-// fs.writeFileSync("./txt/output.txt", textOut);
-
-// // Non-blocking asynchronous way
-// fs.readFile("./txt/start.txt", (err, data1) => {
-//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-//     const textOut2 = `This is what we know about the avocado: ${data2}.\nCreated on ${Date.now()}`;
-//     console.log(`Successfully read file ${data1}.txt`);
-//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-//       console.log("Successfully read file append.txt");
-//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-//         console.log("Writing has finished successfully!");
-//       });
-//     });
-//   });
-// });
-// console.log("Reading started...");
-// ------------FILES------------
-
-// ------------SERVER------------
-
 const templateCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
   "utf-8",
